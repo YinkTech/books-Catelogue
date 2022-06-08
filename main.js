fa2b7705cd45832689ab734f1db4fd08b53f7545
Olayinka Bidemi Ayeni <65237847+YinkTech@users.noreply.github.com>
2022-06-08 17:06:06 +0100

    checking jscript to show

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+function onFormSubmit() {
+    var formData = readFormData ();
+    insertNewRecord(formData);
 
+}
+
+function readFormData() {
+    var formData = {};
+    formData["bookName"] = document.getElementById('bookName').value;
+    formData["bookAuthor"] = document.getElementById('bookAuthor').value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById('bookList').getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innnerHtml = data.bookName;
+    cell2 = newRow.insertCell(1);
+    cell2.innnerHtml = data.bookAuthor;
+    cell3 = newRow.insertCell(2);
+    cell3.innnerHtml = '<a>Delete</a>'; 
+}
